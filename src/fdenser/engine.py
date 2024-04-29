logger introduced. fix key rank calculation on nan predictions

src/fdenser/engine.py
@@ -13,7 +13,9 @@
 # limitations under the License.
 
 
+import logging
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import pickle
 import random
 from copy import deepcopy
@@ -25,6 +27,7 @@ import numpy as np
 
 from .evaluator import Evaluator
 from .execution import (
+    configure_logging,
     pickle_evaluator,
     pickle_population,
     save_pop,
@@ -32,10 +35,8 @@ from .execution import (
 )
 from .individual import Individual
 
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
-import logging
-import tensorflow as tf
-tf.get_logger().setLevel(logging.WARNING)
+
+logger = logging.getLogger(__name__)
 
 
 def select_fittest(population, population_fits, grammar, cnn_eval, gen,
@@ -437,6 +438,9 @@ def main(run, dataset, input_shape, config, grammar):
         random.setstate(pkl_random)
         np.random.set_state(pkl_numpy)
 
+    log_path = run_path / Path('fdenser.log')
+    configure_logging(log_path)
+
     for gen in range(last_gen+1, config["evolutionary"]["num_generations"]):
 
         # check the total number of epochs (stop criteria)
@@ -445,8 +449,8 @@ def main(run, dataset, input_shape, config, grammar):
             break
 
         if gen == 0:
-            print('[%d] Creating the initial population' % (run))
-            print('[%d] Performing generation: %d' % (run, gen))
+            logger.info('[%d] Creating the initial population' % (run))
+            logger.info('[%d] Performing generation: %d' % (run, gen))
 
             # create initial population
             population = [
@@ -464,7 +468,7 @@ def main(run, dataset, input_shape, config, grammar):
                 )
                 for _id_ in range(config["evolutionary"]["lambda"])
             ]
-            print('Population Initialized!')
+            logger.info('Population Initialized!')
 
             # set initial population variables and evaluate population
             population_fits = []
@@ -480,10 +484,10 @@ def main(run, dataset, input_shape, config, grammar):
                     )
                 )
                 ind.id = idx
-            print('Initial Population Evaluated!')
+            logger.info('Initial Population Evaluated!')
 
         else:
-            print('[%d] Performing generation: %d' % (run, gen))
+            logger.info('[%d] Performing generation: %d' % (run, gen))
 
             # generate offspring (by mutation)
             offspring = [
@@ -523,12 +527,10 @@ def main(run, dataset, input_shape, config, grammar):
                 )
                 ind.id = idx
 
-        # select parent
-        print('Selecting Fittest')
         parent = select_fittest(
             population, population_fits, grammar, cnn_eval, gen, run_path,
             config["evolutionary"]["default_train_time"])
-        print('Fittest selected')
+        logger.debug(f'Fittest individual is {parent.id}')
 
         # remove temporary files to free disk space
         if gen > 1:
@@ -549,17 +551,18 @@ def main(run, dataset, input_shape, config, grammar):
             with open(Path(run_path, 'best_parent.pkl'), 'wb') as handle:
                 pickle.dump(parent, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
-        print(f'[{run}] Best fitness of generation {gen}: '
+        logger.info(f'[{run}] Best fitness of generation {gen}: '
               f'{max(population_fits)}')
-        print(f'[{run}] Best overall fitness: {best_fitness}')
+        logger.info(f'[{run}] Best overall fitness: {best_fitness}')
 
         # save population
         save_pop(population, run_path, gen)
         pickle_population(population, parent, run_path)
 
         total_epochs += sum([ind.num_epochs for ind in population])
+        log.debug(f'Total epochs: {total_epochs}')
 
     # compute testing performance of the fittest network
     best_test_acc = cnn_eval.testing_performance(
         str(Path(run_path, 'best.h5')))
-    print('[%d] Best test accuracy: %f' % (run, best_test_acc))
+    logger.info('[%d] Best test accuracy: %f' % (run, best_test_acc))

src/fdenser/individual.py
@@ -1,4 +1,5 @@
 import contextlib
+import logging
 from multiprocessing import Pool
 from time import time
 
@@ -6,6 +7,9 @@ import keras
 import numpy as np
 
 from .module import Module
+
+
+logger = logging.getLogger(__name__)
 
 
 class Individual:
@@ -231,7 +235,7 @@ class Individual:
 
         num_pool_workers = 1
         with contextlib.closing(Pool(num_pool_workers)) as po:
-            print('Pool reached')
+            logger.debug(f'Pool reached for individual {self.id}')
             pool_results = po.map_async(
                 tf_evaluate,
                 [(cnn_eval, phenotype, load_prev_weights,
@@ -239,9 +243,6 @@ class Individual:
                   train_time, self.num_epochs, self.input_shape)]
             )
             metrics = pool_results.get()[0]
-        # metrics = tf_evaluate([cnn_eval, phenotype, load_prev_weights,
-        #     weights_save_path, parent_weights_path, train_time, self.num_epochs, self.input_shape])
-        # print(metrics)
 
         if metrics is not None:
             if 'val_accuracy' in metrics:
@@ -267,7 +268,10 @@ class Individual:
                     ]
             self.metrics = metrics
             if 'accuracy_test' in metrics:
-                if type(self.metrics['accuracy_test']) is float:
+                if self.metrics['accuracy_test'] is None:
+                    self.fitness = -1
+                    logger.warning(f'Failed to evaluate individual {self.id}')
+                elif type(self.metrics['accuracy_test']) is float:
                     self.fitness = self.metrics['accuracy_test']
                 elif type(self.metrics['accuracy_test']) is int:
                     self.fitness = self.metrics['accuracy_test']
@@ -280,6 +284,7 @@ class Individual:
             self.trainable_parameters = self.metrics['trainable_parameters']
             self.current_time += (self.train_time-self.current_time)
         else:
+            logger.warning(f'Failed to train individual {self.id}')
             self.metrics = None
             self.fitness = -1
             self.num_epochs = 0
@@ -288,6 +293,7 @@ class Individual:
 
         self.time = time() - start
 
+        logger.info(f'Fitness of {self.id}: {self.fitness}')
         return self.fitness
 
 
@@ -333,7 +339,7 @@ def tf_evaluate(args):
         gpus = tf.config.experimental.list_physical_devices('GPU')
         tf.config.experimental.set_memory_growth(gpus[0], True)
     except IndexError:
-        print('RUNNING ON CPU ONLY!', flush=True)
+        logger.debug('Evaluation will run on CPU')
 
     cnn_eval, phenotype, load_prev_weights, weights_save_path, \
         parent_weights_path, train_time, num_epochs, input_shape = args
@@ -349,10 +355,10 @@ def tf_evaluate(args):
             input_shape,
         )
     except tf.errors.ResourceExhaustedError:
-        print('Memory Error', flush=True)
+        logger.warning('Memory Error: ResourceExhaustedError')
         keras.backend.clear_session()
         return None
     except TypeError:
-        print('Memory Error', flush=True)
+        logger.warning('Memory Error: TypeError')
         keras.backend.clear_session()
         return None

src/fdenser/sca/metrics.py
@@ -1,6 +1,10 @@
+import logging
 import sys
 import h5py
 import numpy as np
+
+
+logger = logging.getLogger(__name__)
 
 
 # The AES SBox that we will use to compute the rank
@@ -51,6 +55,13 @@ def rank(true_y, predictions, target_byte=2):
     # We do this by sorting our estimates and find the rank in the sorted array.
     sorted_proba = np.array(list(map(lambda a : key_bytes_proba[a], key_bytes_proba.argsort()[::-1])))
     real_key = metadata[0]['key'][target_byte]
-    real_key_rank = np.where(sorted_proba == key_bytes_proba[real_key])[0][0]
-    # engine will try to maximize the fitness so we invert the values
-    return 255 - int(real_key_rank)
+    try:
+        real_key_rank = np.where(sorted_proba == key_bytes_proba[real_key])[0][0]
+        # engine will try to maximize the fitness so we invert the values
+        return 255 - int(real_key_rank)
+    except IndexError:
+        # probably the model output predictions with NaN values thus failing to
+        # compute the rank. This model should be discarded (thus the None
+        # return value)
+        logger.warning('The model generated failed to produce a valid rank metric')
+        return None